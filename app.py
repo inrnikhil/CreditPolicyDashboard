aa8b2c27c6862b9350dbd837dfa13bb5f6b0cd30
Nikhil Raj <nraj.iitkgp@email.com>
2026-05-29 18:59:39 -0400

    Initial commit

---
+++ app.py
@@ -0,0 +1,115 @@
+# app.py
+
+import streamlit as st
+import numpy as np
+
+st.title("Credit Card Policy Engine")
+
+st.sidebar.header("Customer Inputs")
+
+bureau_score = st.sidebar.slider(
+    "Bureau Score",
+    300,
+    850,
+    650
+)
+
+income = st.sidebar.number_input(
+    "Annual Income",
+    value=50000
+)
+
+dti = st.sidebar.slider(
+    "DTI %",
+    0,
+    100,
+    30
+)
+
+utilization = st.sidebar.slider(
+    "Utilization %",
+    0,
+    100,
+    40
+)
+
+past_dq = st.sidebar.number_input(
+    "Past Delinquencies",
+    min_value=0,
+    max_value=10,
+    value=0
+)
+
+age = st.sidebar.slider(
+    "Age",
+    21,
+    75,
+    35
+)
+
+# PD Model
+
+score_effect = -0.015*(bureau_score-640)
+util_effect = 0.02*(utilization-30)
+dti_effect = 0.035*(dti-30)
+dq_effect = past_dq
+
+logit = (
+    -1.2
+    + score_effect
+    + util_effect
+    + dti_effect
+    + dq_effect
+)
+
+pd_prob = 1/(1+np.exp(-logit))
+
+# Decision
+
+if (
+    bureau_score < 550
+    or dti > 60
+    or past_dq >= 3
+    or pd_prob > 0.60
+):
+    decision = "DECLINE"
+else:
+    decision = "APPROVE"
+
+# Risk Tier
+
+if pd_prob <= 0.10:
+    risk_tier = "Prime"
+    credit_line = 15000
+
+elif pd_prob <= 0.20:
+    risk_tier = "Near Prime"
+    credit_line = 8000
+
+elif pd_prob <= 0.35:
+    risk_tier = "Subprime"
+    credit_line = 3000
+
+else:
+    risk_tier = "Deep Subprime"
+    credit_line = 500
+
+st.metric(
+    "Probability of Default",
+    f"{pd_prob:.2%}"
+)
+
+st.metric(
+    "Decision",
+    decision
+)
+
+st.metric(
+    "Risk Tier",
+    risk_tier
+)
+
+st.metric(
+    "Credit Limit",
+    f"${credit_line:,.0f}"
+)
